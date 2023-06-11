54ad34744183079e53d724fe2403c04c36fe7739
KSH <a01027756032@gmail.com>
2023-06-11 09:21:25 +0000

    API 버그 수정(Fix API bugs)

--- REST API/User.js
+++ REST API/User.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
     Username: { type: String, required: true },
     Password: { type: String, required: true },
-});
+}, { versionKey: false });
 
 module.exports = mongoose.model('Users', UserSchema, 'users');
 
